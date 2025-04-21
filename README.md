436f8386b8c92d6dfebba5326383e073e2bf88e4
virtual PC <huxinsheng2015@icloud.com>
2025-04-21 15:56:04 +0800

    init commit

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Timestamp Microservice
 
 This is the boilerplate code for the Timestamp Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
+
+
+## Project Description
+
+This project is a simple microservice that takes a date string as input and returns the Unix timestamp and the human-readable date and time for that input. The service should support two types of input:
+
+
+1. API   /api/:data?     
+2. if the input is invalid, the service should return a JSON object with an error message like: { error: "Invalid date" }.
+3. if the input is valid, the service should return a JSON object with the following format: { unix: 1516914907(type:number), utc: "Thu, 01 Jan 1970 00:00:00 GMT"(type:string)" }.    /api/1516914907    /api/
+4. if the input is not provided, the service should only return the current Unix timestamp.
+5. Your project can handle dates that can be successfully parsed by new Date(date_string).
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,8 +21,79 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
+
+
+// ******************************* My API endpoints ***********************
+
+// 判断:data的状态    什么类型？是否是空格
+
+
+
+
+// function isTimeValid(timeString) {
+//   // 使用Date构造函数尝试解析时间字符串
+//   const date = new Date(timeString);
+
+//   // 如果时间戳是有效的，并且不是NaN，表示时间有效
+//   return !isNaN(date.getTime());
+// }
+
+// // 测试示例
+// const validTime = '2024-01-01T12:00:00';
+// let validTime2 = '2024-01-01T12';
+// const invalidTime = 'invalid time';
+// let invalidTime2 = '';
+
+// console.log(isTimeValid(validTime)); // true
+// console.log(isTimeValid(validTime2)); // true
+// console.log(isTimeValid(invalidTime)); // false
+// console.log(isTimeValid(invalidTime2)); // false
+
+
+
+
+app.use(express.json());
+
+app.get('/api/:date?', (req, res) => {
+  let date = req.params.date;
+  let response = {};
+
+  if (!date) {
+    // 如果date为空，使用当前时间
+    date = new Date();
+  } else {
+    // 尝试解析date
+    if (typeof date === 'string') {
+      date = new Date(date);
+    } else if (typeof date === 'number') {
+      date = new Date(date * 1000); // 假设date是秒级的unix时间戳
+    } else {
+      return res.status(400).json({
+        error: 'Invalid date'
+      });
+    }
+  }
+
+  if (isNaN(date.getTime())) {
+    // 如果date无效，返回错误信息
+    return res.status(400).json({
+      error: 'Invalid date'
+    });
+  }
+
+  // 如果date有效，返回unix时间戳和UTC时间
+  response.unix = Math.floor(date.getTime());
+  response.utc = date.toUTCString();
+
+  res.json(response);
+});
+
+
+
+
+// ******************************* My API endpoints ***********************
 
 
 
